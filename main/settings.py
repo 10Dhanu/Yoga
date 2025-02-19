758d97733b896f6bf68e99ba999aef1a7296a412
Dhanu <dhanashrisubhedar10.ds@gmail.com>
2025-02-19 23:18:23 +0100

    Updated ALLOWED_HOSTS for deployment

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['yogaified.de', 'www.yogified.de']
+ALLOWED_HOSTS = ['yogaified.onnetlify.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
