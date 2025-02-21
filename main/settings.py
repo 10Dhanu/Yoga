e09836114ecb4441293e65b9b1dc95df3d66b72d
Dhanu <dhanashrisubhedar10.ds@gmail.com>
2025-02-21 10:55:14 +0100

    Updated ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['yogaified.onnetlify.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['yoga-xtqw.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
